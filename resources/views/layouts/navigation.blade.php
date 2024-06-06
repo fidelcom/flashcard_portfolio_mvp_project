f42baa888c2cc96219ccc377c4b6b4421144fc96
Fidelcom <okpara2040@gmail.com>
2024-06-06 17:36:19 +0100

    Landing page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,6 +26,11 @@
                         {{ __('Flash Cards') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('card.create')" :active="request()->routeIs('card.create') || request()->routeIs('card.edit')">
+                        {{ __('Add Flash Cards') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
